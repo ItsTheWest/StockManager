25629c1e33303da48c310fa35933c1943453d9aa
Nelson Josue <nelsonjosue0407@gmail.com>
2025-11-29 19:41:01 -0400

    TaildWind
    Ya funciona tailwind

--- frontReact/src/App.tsx
+++ frontReact/src/App.tsx
@@ -1,11 +1,11 @@
-import  {BrowserRouter, Route, Routes,  } from 'react-router-dom';
-import {login} from './assets/pages/login';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Login } from './pages/login';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<login/>} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   )
--- frontReact/src/assets/pages/login.tsx
+++
@@ -1,6 +0,0 @@
-
-export function Login() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">Holaa</div>
-  );
-}
