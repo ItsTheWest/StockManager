ac002e8f8f92158672f8a4bfa481757b05b6c0a5
Nelson Josue <nelsonjosue0407@gmail.com>
2025-12-27 13:47:44 -0400

    Se acaba de acentar la bases de el backend de manera correcta ahora intentare realizar la conexion con una en teoria ya esta conectada la abse de datos

--- backDjango/backDjango/urls.py
+++ backDjango/backDjango/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for App project.
+URL configuration for backDjango project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.0/topics/http/urls/
